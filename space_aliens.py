8d0f0fdc7d3163a444f7bc1cb931ddbf5ad4dca8
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2023-01-10 04:04:22 +0000

    fixed comments

--- space_aliens.py
+++ space_aliens.py
@@ -2,6 +2,23 @@
 
 # Created by: Nathan Araujo
 # Date: Jan.9, 2022
-# This program displays Hello Everyone! Welcome to my game! on the pybadge
+# This program is called "Space Attack program on the PyBadge"
 
-print("Hello Everyone! Welcome to my game!")
+import ugame
+import stage
+
+def game_scene():
+    # 3 blank lines
+    print("\n\n\n")
+    # prints the text "Hello Everyone"
+    print("Hello Everyone!")
+    # prints the text "My name is Nathan"
+    print("My name is Nathan.")
+
+    # repeat forever, game loop
+    while True:
+        # just a place holder for now 
+        pass 
+
+if __name__ == "__main__":
+    game_scene()
