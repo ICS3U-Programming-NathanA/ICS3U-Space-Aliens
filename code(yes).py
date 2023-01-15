82accf6bd453c8c7290cde551de867b9612813e7
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2023-01-15 20:36:52 +0000

    06 code

--- code(yes).py
+++ code(yes).py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
- 
+
 # Created by: Nathan Araujo
 # Date: Jan.9, 2022
 # This program is called "Space Attack" program on the PyBadge
@@ -13,26 +13,26 @@ import constants
 
 
 def game_scene():
- 
+
     # this function is the main game scene
- 
+
     # gets the background image from the file
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     # gets the sprite from the file
     image_bank_sprite = stage.Bank.from_bmp16("space_aliens.bmp")
- 
+
     # buttons you want to keep information on
     a_button = constants.button_state["button_up"]
     b_button = constants.button_state["button_up"]
     start_button = constants.button_state["button_up"]
     select_button = constants.button_state["button_up"]
- 
+
     # get sound ready
     pew_sound = open("pew.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
- 
+
     # create a grid on the pybadge for the background
     background = stage.Grid(
         image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
@@ -53,11 +53,11 @@ def game_scene():
     # take images and add them to a list
     game.layers = [ship] + [alien] + [background]
     game.render_block()
- 
+
     while True:
         # get user input
         keys = ugame.buttons.get_pressed()
- 
+
         # If they press the "A" key
         if keys & ugame.K_X != 0:
             if a_button == constants.button_state["button_up"]:
@@ -99,7 +99,7 @@ def game_scene():
         # If they press the down key
         if keys & ugame.K_DOWN:
             pass
- 
+
         # update game logic
         # play the shoot sound if button "A" is pressed
         if a_button == constants.button_state["button_just_pressed"]:
